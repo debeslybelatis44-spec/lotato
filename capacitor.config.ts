6fbb1ba49507ea2243f2e20fb111779e6724fa35
debeslybelatis44-spec <debeslybelatis44@gmail.com>
2026-04-19 22:52:49 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,18 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: '.lotato.',
+  appName: 'LOTATO Print',
+  webDir: 'www',
+  server: {
+    url: 'https://lotato1.onrender.com',
+    cleartext: true
+  },
+  plugins: {
+    SunmiPrinter: {
+      bindOnLoad: true
+    }
+  }
+};
+
+export default config;
